45357a99facfa9cb8f93cc1b3f3be0a4ebbd063e
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-12-18 12:36:38 +0900

    feat: finish List6-1

---
+++ lib/Chapter_06/BubbleSort.rb
@@ -0,0 +1,33 @@
+require "csv"
+
+module Chapter_06
+	class BubbleSort
+		class << self
+			def excute(array)
+				sort_array = array.clone
+				array_max_num = sort_array.length.to_i - 1
+
+				(0..array_max_num).each do |i|
+					array_max_num.downto(i + 1) do |num|
+						number_to_compare = num - 1
+						if sort_array[number_to_compare] > sort_array[num]
+							sort_array[number_to_compare], sort_array[num] =
+							sort_array[num], sort_array[number_to_compare]
+						end
+					end
+				end
+				sort_array
+			end
+
+			def import_csv
+				dir_pwd = Dir.pwd 
+				file_path = "#{dir_pwd}/lib/Chapter_06/Array.csv"
+				data_list = CSV.read(file_path)
+				array = data_list[0].map { |n| n.to_i}
+				Chapter_06::BubbleSort.excute(array)
+			end
+		end
+	end
+end
+
+p Chapter_06::BubbleSort.import_csv
